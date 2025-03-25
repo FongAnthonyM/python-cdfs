71fbcce093485d035b725d8aa3f965a5433f9a5f
Anthony Fong <FongAnthonyM@gmail.com>
2025-03-24 20:00:01 -0700

    Updated pickling to be more uniform with baseobjects

--- src/cdfs/components/basecdfscontentscomponent.py
+++ src/cdfs/components/basecdfscontentscomponent.py
@@ -65,11 +65,15 @@ class BaseCDFSContentsComponent(BaseCDFSComponent):
             self.construct(composite, table_name, **kwargs)
 
     # Pickling
-    def __getstate__(self) -> dict[str, Any]:
-        """Creates a dictionary of attributes which can be used to rebuild this object.
+    def __getstate__(self) -> None | dict[str, Any] | tuple[dict[str, Any] | None, dict[str, Any]]:
+        """Gets the object's state for pickling.
 
         Returns:
-            dict: A dictionary of this object's attributes.
+            The state returned will be either of the following types based on the presence of __dict__ and __slots__:
+                None: __dict__ nor __slots__ are present.
+                dict: __dict__ is present and __slots__ is not present.
+                tuple[None, dict]: __dict__ is not present and __slots__ is present.
+                tuple[dict, dict]: __dict__ is present and __slots__ is present.
         """
         state = super().__getstate__()
         for name in ("_contents_table",):
